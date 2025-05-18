20531ae28494b962f98482ae5150fa64df744a61
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-18 20:17:39 +0200

    Fixed spelling mistake

--- src/services/getCharactersApi.js
+++ src/services/getCharactersApi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export async function getCharactesApi(){
+export async function getCharactersApi() {
   const apiUrl = "https://dragonball-api.com/api/characters";
 
   try {
     const response = await axios.get(apiUrl);
-    return response.data.items
+    return response.data.items;
   } catch (error) {
-    console.error("Error al importar los personajes", error)
-    throw error
+    console.error("Error al importar los personajes", error);
+    throw error;
   }
 }
