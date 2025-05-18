f2aea8558121af94689414996ff2c3df6f7b2346
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-18 19:39:07 +0200

    Create search bar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [name, setName] = useState("");
+  const [minKi, setMinKi] = useState("");
+  const [maxKi, setMaxKi] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Buscar por nombre
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Introduce un nombre"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="form-control"
+        />
+
+        <label htmlFor="minKi" className="form-label">
+          Buscar por ki
+        </label>
+        <input
+          id="minKi"
+          type="number"
+          placeholder="De"
+          value={minKi}
+          onChange={(e) => setMinKi(e.target.value)}
+          className="form-control"
+        />
+
+        <label htmlFor="maxKi" className="form-label"></label>
+        <input
+          id="maxKi"
+          type="number"
+          placeholder="A"
+          value={maxKi}
+          onChange={(e) => setMaxKi(e.target.value)}
+          className="form-control"
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Buscar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
